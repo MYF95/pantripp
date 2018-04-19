9751c3db2b7c8cb131bcd823f1e905ac6e868ac0
Mikota <myf1995@gmail.com>
2018-04-19 10:21:52 +0100

    refactored some of the lists logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:lists]
   before_action :user_getter, only: [:show, :lists]
+  before_action :userlist_getter, only: [:show, :lists]
 
   def show; end
 
   def lists
     if current_user?(@user)
-      @users = @user.lists.paginate(page: params[:page], per_page: 9)
     else
+      # Redirects user to their lists if they try to get into another users' list
       @user = current_user
       @users = @user.lists.paginate(page: params[:page], per_page: 9)
       redirect_to user_list_path(@user)
@@ -18,6 +19,10 @@ class UsersController < ApplicationController
 
     def user_getter
       @user = User.find(params[:id])
+    end
+
+    def userlist_getter
+      @users = @user.lists.paginate(page: params[:page], per_page: 9)
     end
 
     def current_user? (user)
