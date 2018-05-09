9606839ed9c7dc412e1f0c0ce0da4903db5c2d47
Mikota <myf1995@gmail.com>
2018-05-09 16:41:51 +0100

    made header and rest of the page responsive

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     window.setTimeout(function() {
         $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
-            $(this).remove();
+            $(this).alert('close');
         });
     }, 3000);
 });
