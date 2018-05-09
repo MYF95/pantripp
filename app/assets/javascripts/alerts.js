601509136d97cd83e8bca02c3a83c537bcca3910
Mikota <myf1995@gmail.com>
2018-05-09 17:00:35 +0100

    minor fixes and TODO cleanups

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,5 +1,3 @@
-/* TODO Logout alert doesn't fade away automatically */
-
 $(document).ready(function () {
     window.setTimeout(function() {
         $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
